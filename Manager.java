d5b271cdfb6a1164a73eb0b83fdfb068541de1a1
Guillaume Moreau <guillaume.moreau@ec-nantes.fr>
2012-10-10 09:12:23 +0200

    ajout d'une classe Manager

---
+++ Manager.java
@@ -0,0 +1,52 @@
+import java.util.LinkedList;
+
+/**
+ * 
+ */
+
+/**
+ * @author moreau
+ * une classe Manager
+ */
+public class Manager extends Employe {
+	/**
+	 * liste des personnes sous les ordres du Manager
+	 */
+	private LinkedList<Employe> employes;
+	
+	/**
+	 * suprieur hirarchique
+	 * 
+	 * pour le patron de l'entreprise, le suprieur vaudra null
+	 */
+	private Manager manager;
+	
+	/**
+	 * @return the manager
+	 */
+	public Manager getManager() {
+		return manager;
+	}
+
+	/**
+	 * @param manager the manager to set
+	 */
+	public void setManager(Manager manager) {
+		this.manager = manager;
+	}
+
+	/**
+	 * @param nom
+	 * @param prenom
+	 * @param sexe
+	 * @param anneeNaissance
+	 * @param numero
+	 * @param entreprise
+	 */
+	public Manager(String nom, String prenom, boolean sexe, int anneeNaissance,
+			int numero, Entreprise entreprise) {
+		super(nom, prenom, sexe, anneeNaissance, numero, entreprise);
+		this.employes = new LinkedList<Employe>();
+	}
+
+}
